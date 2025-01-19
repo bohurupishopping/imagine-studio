24d1a263f10e5dd978d9d0a12ecc699bb4899a9b
Bohurupi Shopping <official.bohurupi@gmail.com>
2025-01-19 17:45:44 +0530

    fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -6,22 +6,96 @@ export const LOCAL_FONTS = {
     weight: 'bold',
     style: 'normal'
   },
-  LiShohid: {
-    name: 'Li Shohid Tahmid',
-    label: 'Li Shohid',
-    path: '/font/Li Shohid Tahmid Tamin Unicode.ttf',
+  Abdullah: {
+    name: 'Abdullah',
+    label: 'Abdullah Lalmonirhat',
+    path: '/font/Abdullah Lalmonirhat.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  AlQuds: {
+    name: 'Al Quds',
+    label: 'Al Quds V2',
+    path: '/font/Al Quds V2.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  AlinurSirat: {
+    name: 'Alinur Sirat',
+    label: 'Alinur Sirat',
+    path: '/font/Alinur Sirat.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  BrutalHonesty: {
+    name: 'Brutal Honesty',
+    label: 'Brutal Honesty',
+    path: '/font/Brutal Honesty Demo.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  DirtyBrush: {
+    name: 'Dirty Brush',
+    label: 'Dirty Brush',
+    path: '/font/DirtyBrush.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  Durbar: {
+    name: 'Durbar',
+    label: 'Durbar Regular',
+    path: '/font/Durbar-Regular-400.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  Liakat: {
+    name: 'Liakat',
+    label: 'Liakat Kalikapur',
+    path: '/font/Liakat Kalikapur.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  Maturasc: {
+    name: 'Maturasc',
+    label: 'Maturasc',
+    path: '/font/MATURASC.TTF',
+    weight: 'normal',
+    style: 'normal'
+  },
+  Mypuma: {
+    name: 'Mypuma',
+    label: 'Mypuma',
+    path: '/font/MYPUMA_.TTF',
+    weight: 'normal',
+    style: 'normal'
+  },
+  Niladri: {
+    name: 'Niladri',
+    label: 'Niladri Nodia',
+    path: '/font/Niladri Nodia.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  ShohidTahmid: {
+    name: 'Shohid Tahmid',
+    label: 'Shohid Tahmid Tamin',
+    path: '/font/Shohid Tahmid Tamin.ttf',
+    weight: 'normal',
+    style: 'normal'
+  },
+  Shokuntola: {
+    name: 'Shokuntola',
+    label: 'Shokuntola UNICODE',
+    path: '/font/Shokuntola UNICODE.ttf',
     weight: 'normal',
     style: 'normal'
   }
 };
 
-export const FONT_OPTIONS = [
-  { value: LOCAL_FONTS.Roundkey.name, label: LOCAL_FONTS.Roundkey.label },
-  { value: LOCAL_FONTS.LiShohid.name, label: LOCAL_FONTS.LiShohid.label },
-  { value: 'Noto Sans Bengali', label: 'Noto Sans' },
-  { value: 'Hind Siliguri', label: 'Hind Siliguri' },
-  { value: 'Kalpurush', label: 'Kalpurush' }
-];
+export const FONT_OPTIONS = Object.values(LOCAL_FONTS).map(font => ({
+  value: font.name,
+  label: font.label
+}));
 
 export const generateFontFaceStyles = () => {
   return Object.values(LOCAL_FONTS)
